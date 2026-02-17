3742cefdfb13a3a6a20083c28f8c2a592d3d677f
Hripsime <hr.hovhanisyan@ispras.ru>
2026-02-17 11:39:19 +0400

    Renamed --gcc-wrapper to --gcc-wrapper-bin

--- tools/tune_project_gcc_wrapper.py
+++ tools/tune_project_gcc_wrapper.py
@@ -12,7 +12,7 @@ argparser = argparse.ArgumentParser(parents=opentuner.argparsers())
 argparser.add_argument('--project-dir', help='Path to the project directory', required=True)
 argparser.add_argument('--compiler-bin', help='Path to the compiler bin directory', required=True)
 argparser.add_argument('--project-binary', help='Name of the project binary', required=True)
-argparser.add_argument('--gcc-wrapper', help='Path to the gcc wrapper script directory', required=True)
+argparser.add_argument('--gcc-wrapper-bin', help='Path to the gcc wrapper bin directory', required=True)
 argparser.add_argument('--optimization-entries', help='Path to the optimization entries file', required=True)
 argparser.add_argument('--output-dir', help='Path to the output directory', required=True)
 argparser.add_argument('--timeout', help='Program running timeout in seconds', type=int, default=10)
@@ -29,8 +29,8 @@ def main():
     args = argparser.parse_args()
 
     # Can be replaced with any Builder or Runner
-    base_builder = CMakeProjectBuilder(args.gcc_wrapper, args.project_dir, args.output_dir, args.project_binary, 7)
-    builder = WrapperEnhancedBuilder(base_builder, args.gcc_wrapper, args.compiler_bin, args.output_dir)
+    base_builder = CMakeProjectBuilder(args.gcc_wrapper_bin, args.project_dir, args.output_dir, args.project_binary, 7)
+    builder = WrapperEnhancedBuilder(base_builder, args.gcc_wrapper_bin, args.compiler_bin, args.output_dir)
     runner = AveragingRunner(BinaryFileRunner(args.timeout, args.cmd_args))
 
     optimization_entries = load_json(args.optimization_entries)
