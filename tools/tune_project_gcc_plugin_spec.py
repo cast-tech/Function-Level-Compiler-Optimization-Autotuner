aa6e860921117793d42a591180598cc03feb675f
Hripsime <hr.hovhanisyan@ispras.ru>
2026-02-17 11:40:48 +0400

    Added scripts to tune the SPEC benchmarks at both the file and function levels

---
+++ tools/tune_project_gcc_plugin_spec.py
@@ -0,0 +1,45 @@
+import argparse
+import opentuner
+import json
+
+from tools.implementations.builders.spec_builder import SPECBuilder
+from tools.implementations.runners.spec_runner import SPECRunner
+from tools.implementations.runners.averaging_runner import AveragingRunner
+from tools.services.iterative_tuner import iterative_tune
+from tools.services.gcc_plugin_support import PluginEnhancedBuilder
+
+argparser = argparse.ArgumentParser(parents=opentuner.argparsers())
+argparser.add_argument('--spec-root', help='Path to the spec root directory', required=True)
+argparser.add_argument('--spec-benchmark', help='Name of the spec benchmark', required=True)
+argparser.add_argument('--spec-config', help='Path to the spec config file', required=True)
+argparser.add_argument('--spec-threads-count', help='Number of threads for the spec run', type=int, default=1)
+argparser.add_argument('--spec-iterations-count', help='Number of iterations for the spec run', type=int, default=1)
+argparser.add_argument('--spec-size', help='Size of the spec run', choices=['test', 'train', 'refspeed'], default='test')
+argparser.add_argument('--spec-core-count', help='Number of cores for the spec build', type=int, default=1)
+argparser.add_argument('--compiler-bin', help='Path to the compiler bin directory', required=True)
+argparser.add_argument('--gcc-plugin', help='Path to the gcc plugin .so file', required=True)
+argparser.add_argument('--optimization-entries', help='Path to the optimization entries file', required=True)
+argparser.add_argument('--output-dir', help='Path to the output directory', required=True)
+argparser.add_argument('--timeout', help='Program running timeout in seconds', type=int, default=10)
+
+
+def load_json(json_path):
+    with open(json_path, 'r') as f:
+        json_content = json.load(f)
+    return json_content
+
+
+def main():
+    args = argparser.parse_args()
+
+    # Can be replaced with any Builder or Runner
+    base_builder = SPECBuilder(args.spec_root, args.spec_benchmark, args.spec_config, args.output_dir, args.spec_core_count, args.compiler_bin)
+    builder = PluginEnhancedBuilder(base_builder, args.gcc_plugin, args.output_dir)
+    runner = AveragingRunner(SPECRunner(args.spec_threads_count, args.spec_iterations_count, args.spec_size, args.timeout))
+
+    optimization_entries = load_json(args.optimization_entries)
+    iterative_tune(args, runner, builder, optimization_entries, args.output_dir)
+
+
+if __name__ == "__main__":
+    main()
